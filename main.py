8282c7ab4a6fd6e8590847758010d96f90b009fe
lauhkz <lautaro_RL@outlook.com>
2024-09-19 09:25:30 -0300

    rotate player function

--- main.py
+++ main.py
@@ -3,21 +3,26 @@
 # throughout this file
 from constants import *
 import pygame
+from player import Player
 
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(000000)
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+
         # even if i call it in the assign of the dt variable, still works
         # clock.tick(60)
         dt = clock.tick(60) / 1000
