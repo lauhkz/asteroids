28b3dc06955d9d3eb5252215449c4ce168858044
lauhkz <lautaro_RL@outlook.com>
2024-09-19 10:49:38 -0300

    added asteroids, and collision feature

--- circleshape.py
+++ circleshape.py
@@ -14,6 +14,12 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
+    def collisions(self, circle):
+        distance = self.position.distance_to(circle.position)
+        if distance <= self.radius + circle.radius:
+            print("Game Over!")
+            exit()
+
     def draw(self, screen):
         # sub-classes must override
         pass
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
 from constants import *
 import pygame
 from player import Player
@@ -10,7 +13,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable
+    Asteroid.containers = (asteroids_group, updatable, drawable)
+    Player.containers = (updatable, drawable)
+
+    asteroidsfield = AsteroidField()
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -19,8 +33,16 @@ def main():
                 return
 
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for unit in updatable:
+            unit.update(dt)
+
+        for unit in asteroids_group:
+            unit.collisions(player)
+
+        for unit in drawable:
+            unit.draw(screen)
+
         pygame.display.flip()
 
         # even if i call it in the assign of the dt variable, still works
